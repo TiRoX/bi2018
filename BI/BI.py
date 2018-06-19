779e64f4b3f8a463957b372c729095d6185cdbad
tirox <k3v0815@web.de>
2018-06-20 00:37:19 +0200

    updated to train_prepared

--- BI/BI.py
+++ BI/BI.py
@@ -12,20 +12,21 @@ from pandas.io.parsers import read_csv
 
 def readF(var):
 
-    output = read_csv(var, delimiter= ',', header = 0, dtype={"Dates": str,  "Descript": str, "DayOfWeek": str, "PdDistrict": str, "Resolution": str, "Address": str, "X": str, "Y": str}) # type = pandas.core.frame.DataFrame
+    output = read_csv(var, delimiter= ';', header = 0, dtype={"Date": str, "Time": str, "Year": int, "Month": int, "Day": int, "Hour": int, "Season": str,  "Descript": str, "DayOfWeek": str, "PdDistrict": str, "Resolution": str, "Address": str, "AdressSuffix": str, "X": str, "Y": str}) # type = pandas.core.frame.DataFrame
 
+    #OUTDATED
     #split Dates
-    output['Date'], output['Time'] = output['Dates'].str.split(' ', 1).str
+    #output['Date'], output['Time'] = output['Dates'].str.split(' ', 1).str
 
     with pa.option_context('display.max_rows', 11, 'display.max_columns', 200):
-        print(output.ix[257059]) # --> Einige Zeilen sind abgeschnitten und ergeben nicht immer viel Sinn. So wie diese hier; Excel index + 2 = Python,,, index 257061 = 257059
-        #print(output[['Date', 'Time', 'Category', 'Descript', 'DayOfWeek', 'PdDistrict', 'Resolution', 'Address','X','Y' ]])
+        #print(output.ix[257059]) # --> Einige Zeilen sind abgeschnitten und ergeben nicht immer viel Sinn. So wie diese hier; Excel index + 2 = Python,,, index 257061 = 257059
+        print(output)
         
         # Abfrage für bestimmten Wert in Spalte
         #print(output.loc[output['Resolution'] == 'NONE'])
         
         #Will suchen nach 'OWNING' im Feld 'Descript'; um das zu tun müssen ggf. Descript Felder in Liste umgewandelt werden. :)
-        print(output.loc[output['Descript'].isin('OWNING')])
+        #print(output.loc[output['Descript'].isin('OWNING')])
         
         #further use: https://www.shanelynn.ie/using-pandas-dataframe-creating-editing-viewing-data-in-python/
         
@@ -39,4 +40,5 @@ def readF(var):
         #return output
 
 #Class has to be in same directory as the .csv
-readF('train.csv')
+#readF('train.csv')
+readF('train_prepared.csv')
