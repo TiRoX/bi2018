a3d4f13af2c22927188998bf0a1d00d7e59268f1
tirox <k3v0815@web.de>
2018-06-20 00:15:06 +0200

    First adds for our Project
    added several features to work with the Dataframe and get a general idea of how it works

---
+++ BI/BI.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 18.06.2018
+
+@author: Kevin
+'''
+
+import pandas as pa
+from pandas.io.parsers import read_csv
+
+
+
+def readF(var):
+
+    output = read_csv(var, delimiter= ',', header = 0, dtype={"Dates": str,  "Descript": str, "DayOfWeek": str, "PdDistrict": str, "Resolution": str, "Address": str, "X": str, "Y": str}) # type = pandas.core.frame.DataFrame
+
+    #split Dates
+    output['Date'], output['Time'] = output['Dates'].str.split(' ', 1).str
+
+    with pa.option_context('display.max_rows', 11, 'display.max_columns', 200):
+        print(output.ix[257059]) # --> Einige Zeilen sind abgeschnitten und ergeben nicht immer viel Sinn. So wie diese hier; Excel index + 2 = Python,,, index 257061 = 257059
+        #print(output[['Date', 'Time', 'Category', 'Descript', 'DayOfWeek', 'PdDistrict', 'Resolution', 'Address','X','Y' ]])
+        
+        # Abfrage für bestimmten Wert in Spalte
+        #print(output.loc[output['Resolution'] == 'NONE'])
+        
+        #Will suchen nach 'OWNING' im Feld 'Descript'; um das zu tun müssen ggf. Descript Felder in Liste umgewandelt werden. :)
+        print(output.loc[output['Descript'].isin('OWNING')])
+        
+        #further use: https://www.shanelynn.ie/using-pandas-dataframe-creating-editing-viewing-data-in-python/
+        
+        #duplicates?
+        #print (output.duplicated(subset='Dates', keep=False)) #Keep=False markiert alle Duplikate als True, keep=first, nur den ersten nicht
+        
+        #selecting
+        #print(output.ix[output['Resolution'] != 'NONE'])
+        #removing
+        #print(output.ix[~(output['Resolution'] != 'NONE')])
+        #return output
+
+#Class has to be in same directory as the .csv
+readF('train.csv')
