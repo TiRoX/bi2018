b5b9b094308ea4f8d5a57ebad52b1f37017057d9
tirox <k3v0815@web.de>
2018-06-21 12:46:42 +0200

    einlesen der datei in readF

--- BI/BI_SanFran_ChiSquare.py
+++ BI/BI_SanFran_ChiSquare.py
@@ -7,12 +7,38 @@ import scipy.stats as stats
 from scipy.stats import chi2_contingency #für chi²
 
 #Import of Train.csv
-#
-df = pd.read_csv("C:/Users/fmert/Python_CodeComa/train.csv", delimiter= ',', header = 0, error_bad_lines=False) # , dtype={"Date": str, "Time": str, "Year": int, "Month": int, "Day": int, "Hour": int, "Season": str,  "Descript": str, "DayOfWeek": str, "PdDistrict": str, "Resolution": str, "Address": str, "AdressSuffix": str, "X": str, "Y": str} columns mit (delimiter";"), die headzeile ist die 0., dtype bestimmt datentyp der Columns
-#print(df) #Dataframe als rohe Tabelle
+def readF(var):
+    df = pd.read_csv(var, delimiter= ',', header = 0, error_bad_lines=False) # , dtype={"Date": str, "Time": str, "Year": int, "Month": int, "Day": int, "Hour": int, "Season": str,  "Descript": str, "DayOfWeek": str, "PdDistrict": str, "Resolution": str, "Address": str, "AdressSuffix": str, "X": str, "Y": str} columns mit (delimiter";"), die headzeile ist die 0., dtype bestimmt datentyp der Columns
+    #OUTDATED
+    #Feld "Dates" zu "Date" und "Time" teilen:
+    #output['Date'], output['Time'] = output['Dates'].str.split(' ', 1).str
+
+    with pd.option_context('display.max_rows', 11, 'display.max_columns', 200):
+        #print(output.ix[257059]) # --> Einige Zeilen sind abgeschnitten und ergeben nicht immer viel Sinn. So wie diese hier; Excel index + 2 = Python,,, index 257061 = 257059
+        print(df)
+
+        # Abfrage für bestimmten Wert "NONE" in Spalte "Resolution"
+        #print(output.loc[output['Resolution'] == 'NONE'])
+
+        #Entfernt alle Einträge "NONE" aus der Spalte "Resolution"
+        #print(output.ix[~(output['Resolution'] != 'NONE')])
+
+
+        #Will suchen nach 'OWNING' im Feld 'Descript'; um das zu tun müssen ggf. Descript Felder in Liste umgewandelt werden. oider einzelnd in CSV ausgelesen werden
+        #print(output.loc[output['Descript'].isin('OWNING')])
+
+        #Viele kompakte leicht zu verstehende Informationen auf Code Basis sind hier zu finden -v
+        #further use: https://www.shanelynn.ie/using-pandas-dataframe-creating-editing-viewing-data-in-python/
+
+        #existieren duplicates?
+        #print (output.duplicated(subset='Dates', keep=False)) #Keep=False markiert alle Duplikate als True, keep=first, nur den ersten nicht
+
+        #Gebe den Dataframe zurück, da wir nun alle Daten in der CSV wie gewünscht bearbeitet haben
+        return df
         
 #Data Understanding
 #
+df = readF('train_prepared.csv')
 df.shape
 df.dtypes #Überprüfen ob alle Spalten nur aus Einträgen eines Typs bestehen
 
